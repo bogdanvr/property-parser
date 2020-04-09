1dcba71e88f343e6147aa9991a57b8f55576937e
bogdanvr <provisionim@gmail.com>
2020-04-09 11:50:13 +0600

    ​Cleaning and adding code comments

--- eraparsepage.py
+++ eraparsepage.py
@@ -2,17 +2,18 @@
 """
 Created on Fri Oct 25 22:58:11 2019
 
-@author: taisiya
+@author: bogdan
 """
-
+"""
+scraper for the automatic collection of product properties from the supplier’s website.
+Unloading properties in excel, in a format for automatic loading in 1C
+"""
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 from urllib.error import HTTPError
-import re
 import ssl
 from datetime import datetime
 import time
-from random import randint
 import pandas as pd
 now = datetime.now()
 date = str(now).split(' ')[0]
@@ -51,20 +52,23 @@ def get_page(url):
     global prod
     global pp
     
+    # try to get url
     try:
-        #url = 'http://www.eraworld.ru'
+        
         
         html = urlopen(url, context=ctx)
-    
+        #url = 'http://www.eraworld.ru'
         
     except HTTPError:
         return print('no html')
+    
+    #try to get html
     try:
         bsobj = bs(html, 'html.parser')
     except HTTPError:
         return print('no bsobj')
     
-    
+    # try to get header h1 and vendore code
     try:
         head = bsobj.h1.get_text()
         hs = head.split('  ')
@@ -76,7 +80,7 @@ def get_page(url):
     except AttributeError:
         print('No header')
     
-    
+    # try to get properties of the product and add them to the dictionary
     try:
         p = {}
         properties = bsobj.find('div', {'id':'itemProporties'})
@@ -95,33 +99,19 @@ def get_page(url):
     
             
             
-#i = 'http://www.eraworld.ru/catalog/32207'
-            
-#get_page(i)          
-            
-       
-    #try:
-     #   for i in bsobj.find_all('div', {'class':'bxr-element-image'}):
-      #      img = i.find('img').attrs['src']
-       #     link = i.find('a').attrs['href']
-        #    
-         #   if img not in images:
-          #      images.add(img)
-           #     link_img.add('{} - {}'.format(link,img))
-  #  except AttributeError:
-   #     print('No img')
 
 
 
+# Open the file with categiries for scraping
 with open('urlera.txt') as f:
     st = f.readlines()
-    
+
+#remove the domain name    
 st = st[0].split('http://www.eraworld.ru')
 print(len(st))
 
-#get_page('/catalog/puskateli_kontaktory_i_rele/rele_elektroteplovoe_rtn_5372_110_135a_tdm/')
 
-     
+# run the scraper on the list
 for i in st:
     count += 1
     print(count)
@@ -130,42 +120,12 @@ for i in st:
 
 
 
-                
-"""  
-
-html = urlopen('https://www.electrostyle.org/catalog/schetchiki_elektroenergii/', context=ctx)
-bsobj = bs(html, 'html.parser')
-try:
-    if bsobj.find('div', {'class':'promo'}):
-        print('Find text')
-    else:
-        print('No text')
-except AttributeError:
-        print('No text')
-"""
-
-#import eraparspage
-
-
-df = pd.DataFrame({'Data':[10, 20, 30, 20, 15, 30, 45], 'Date':[25,36,78,32,99,14,23] })
-
-dfd = pd.DataFrame({'name':['fonar', 'zazhim'], 'garant':[5, 4], 'massa':[0.14, 0.12], 'napr':[1, 10]})
-
-
-
-writer = pd.ExcelWriter('pandas_simple.xlsx', engine='xlsxwriter')
-
-
-df.to_excel(writer, sheet_name='Sheet1')
-
-# Close the Pandas Excel writer and output the Excel file.
-writer.save()
 
 dd = {}
 
 
 
- 
+# Get all the property headers that will be used as table columns
 s = [list(i) for i in pp]
 k = set()
 
@@ -176,10 +136,8 @@ for i in s:
 klist = list(k)
 print('klist =', klist)
 
-"""
-for i in klist:
-    dd[i] = [ x[i] for x in m]
-"""
+
+# We get a dictionary in which the key is the name of the property, and the value is a list of all its values
 
 for i in klist:
     dd[i] = [ x.get(i) for x in pp]
@@ -187,6 +145,8 @@ for i in klist:
     
     
 print(dd)
+
+# Writing a dictionary in a dataframe Pandas
 
 df2 = pd.DataFrame(dd)
 print(df2)
